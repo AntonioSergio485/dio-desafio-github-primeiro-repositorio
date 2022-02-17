5be9fbeb187252cc1f874ba33d1aa7471247f1d8
AntonioSergio485 <56229735+AntonioSergio485@users.noreply.github.com>
2022-02-17 12:13:59 -0300

    Add files via upload

---
+++ Construtores/ExemploConstrutores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Log log = Log.GetInstance();
+            log.PropriedadeLog = "Teste Log";
+
+            Log log2 = Log.GetInstance();
+            System.Console.WriteLine(log2.PropriedadeLog);
+
+            // Pessoa p1 = new Pessoa();
+            // p1.apresentar();
+        }
+    }
+}
